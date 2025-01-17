52e0fed0e4fbabd4222e7aab10585d6e7652bb09
BejonaSpahija <besjonaspahija1@gmail.com>
2025-01-17 12:12:33 +0100

    First commit

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class CalculatorTest {
+
+    Calculator calculator;
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void add() {
+        assertEquals(7, calculator.add(3, 4));
+        assertNotEquals(7, calculator.add(3, 3));
+    }
+
+    @Test
+    void subtract(){
+        assertEquals(1, calculator.subtract(5, 4));
+        assertNotEquals(1, calculator.subtract(5, 3));
+    }
+
+    @Test
+    void multiply(){
+        assertEquals(12, calculator.multiply(3, 4));
+        assertNotEquals(12, calculator.multiply(3, 3));
+    }
+
+    @Test
+    void divide(){
+        assertEquals(3, calculator.divide(9, 3));
+        assertNotEquals(4, calculator.divide(9, 3));
+    }
+
+    @Test
+    void divideByZero(){
+        assertThrows(ArithmeticException.class, () -> calculator.divide(3, 0));
+    }
+}
